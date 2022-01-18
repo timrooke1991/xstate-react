191dd145d2723c6623fbbca77ba1b3bf99e14d5c
Tim Rooke <timrooke1991@gmail.com>
2022-01-18 11:35:17 +0000

    xstate exercise 00

--- src/00/Timer.js
+++ src/00/Timer.js
@@ -5,10 +5,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ProgressCircle } from '../ProgressCircle';
 
 // Import the timer machine and its initial state:
-// import { ... } from './timerMachine';
+import { timerMachine, timerMachineConfig } from './timerMachine';
 
 export const Timer = () => {
-  const state = ''; // delete me - useReducer instead!
+  const [state, dispatch] = useReducer(timerMachine, timerMachineConfig.initialState);
 
   const { duration, elapsed, interval } = {
     duration: 60,
@@ -36,7 +36,7 @@ export const Timer = () => {
         <div
           className="elapsed"
           onClick={() => {
-            // ...
+            dispatch({ type: 'TOGGLE' })
           }}
         >
           {Math.ceil(duration - elapsed)}
@@ -44,7 +44,7 @@ export const Timer = () => {
         <div className="controls">
           <button
             onClick={() => {
-              // ...
+              dispatch({ type: 'RESET' })
             }}
           >
             Reset
@@ -52,23 +52,27 @@ export const Timer = () => {
         </div>
       </div>
       <div className="actions">
-        <button
-          onClick={() => {
-            // ...
-          }}
-          title="Pause timer"
-        >
-          <FontAwesomeIcon icon={faPause} />
-        </button>
-
-        <button
-          onClick={() => {
-            // ...
-          }}
-          title="Start timer"
-        >
-          <FontAwesomeIcon icon={faPlay} />
-        </button>
+        {
+          state === 'running' ? (
+            <button
+              onClick={() => {
+                dispatch({ type: 'TOGGLE' })
+              }}
+              title="Pause timer"
+            >
+              <FontAwesomeIcon icon={faPause} />
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                dispatch({ type: 'TOGGLE' })
+              }}
+              title="Start timer"
+            >
+              <FontAwesomeIcon icon={faPlay} />
+            </button>
+          )
+        }
       </div>
     </div>
   );
--- src/00/timerMachine.js
+++ src/00/timerMachine.js
@@ -1,13 +1,26 @@
 export const timerMachineConfig = {
-  // ...
+  initialState: 'idle',
+  states: {
+    idle: {
+      on: {
+        TOGGLE: 'running'
+      }
+    },
+    paused: {
+      on: {
+        TOGGLE: 'running',
+        RESET: 'idle'
+      }
+    },
+    running: {
+      on: {
+        TOGGLE: 'paused'
+      }
+    }
+  }
 };
 
 export const timerMachine = (state, event) => {
-  // Add the logic that will read the timerMachineConfig
-  // and return the next state, given the current state
-  // and event received
-
-  // ...
-
-  return state;
+  console.log(state, event);
+  return timerMachineConfig.states[state]?.on?.[event.type] || state;
 };
