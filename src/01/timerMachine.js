985ee6da50fcd5ad721bae74ae5441ad185379ed
Tim Rooke <timrooke1991@gmail.com>
2022-01-19 08:05:03 +0000

    Exercise 01 - adding xstate/react and useMachine

--- src/01/timerMachine.js
+++ src/01/timerMachine.js
@@ -2,3 +2,25 @@ import { createMachine } from 'xstate';
 
 // Use the machine you created in Exercise 00
 // export const timerMachine = // ...
+
+export const timerMachine = createMachine({
+  initial: 'idle',
+  states: {
+    idle: {
+      on: {
+        TOGGLE: 'running'
+      }
+    },
+    paused: {
+      on: {
+        TOGGLE: 'running',
+        RESET: 'idle'
+      }
+    },
+    running: {
+      on: {
+        TOGGLE: 'paused'
+      }
+    }
+  }
+});
