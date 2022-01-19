7d404987b9df23385acba243372d6c667a96d11b
Tim Rooke <timrooke1991@gmail.com>
2022-01-19 09:02:04 +0000

    Exercise 03 - adding TICK event

--- src/03/timerMachine.js
+++ src/03/timerMachine.js
@@ -1,9 +1,18 @@
 import { createMachine, assign } from 'xstate';
 
 // Parameterize the assign actions here:
-// const tick = ...
-// const addMinute = ...
-// const reset = ...
+const tick = assign({
+  duration: (ctx) => ctx.duration - 1,
+});
+
+const addMinute = assign({
+  duration: (ctx) => ctx.duration + 60,
+});
+
+const reset = assign({
+  duration: 60,
+  elapsed: 0,
+});
 
 export const timerMachine = createMachine({
   initial: 'idle',
@@ -15,26 +24,19 @@ export const timerMachine = createMachine({
   states: {
     idle: {
       // Parameterize this action:
-      entry: assign({
-        duration: 60,
-        elapsed: 0,
-      }),
-
+      entry: reset,
       on: {
         TOGGLE: 'running',
       },
     },
     running: {
       on: {
-        // On the TICK event, the context.elapsed should be incremented by context.interval
-        // ...
-
+        TICK: {
+          actions: tick
+        },
         TOGGLE: 'paused',
         ADD_MINUTE: {
-          // Parameterize this action:
-          actions: assign({
-            duration: (ctx) => ctx.duration + 60,
-          }),
+          actions: addMinute
         },
       },
     },
@@ -44,5 +46,5 @@ export const timerMachine = createMachine({
         RESET: 'idle',
       },
     },
-  },
+  }
 });
