fd210278c9fdc7fedbdef11a4e4fae38c1c4325b
Tim Rooke <timrooke1991@gmail.com>
2022-01-19 08:26:30 +0000

    Exercise 03 - adding actions and entry states

--- src/02/Timer.js
+++ src/02/Timer.js
@@ -9,12 +9,7 @@ import { ProgressCircle } from '../ProgressCircle';
 export const Timer = () => {
   const [state, send] = useMachine(timerMachine);
 
-  // Use state.context instead
-  const { duration, elapsed, interval } = {
-    duration: 60,
-    elapsed: 0,
-    interval: 0.1,
-  };
+  const { duration, elapsed, interval } = state.context;
 
   return (
     <div
@@ -41,13 +36,15 @@ export const Timer = () => {
             <button onClick={() => send('RESET')}>Reset</button>
           )}
 
-          <button
-            onClick={() => {
-              // ...
-            }}
-          >
-            + 1:00
-          </button>
+          {state.value === 'running' && (
+            <button
+              onClick={() => {
+                send({ type: 'ADD_MINUTE' })
+              }}
+            >
+              + 1:00
+            </button>
+          )}
         </div>
       </div>
       <div className="actions">
--- src/02/timerMachine.js
+++ src/02/timerMachine.js
@@ -4,12 +4,17 @@ export const timerMachine = createMachine({
   initial: 'idle',
   // Add initial context
   // ...
-
+  context: {
+    duration: 60,
+    elapsed: 0,
+    interval: 0.1
+  },
   states: {
     idle: {
-      // Reset duration and elapsed on entry
-      // ...
-
+      entry: assign({
+        duration: 60,
+        elapsed: 0,
+      }),
       on: {
         TOGGLE: 'running',
       },
@@ -17,9 +22,11 @@ export const timerMachine = createMachine({
     running: {
       on: {
         TOGGLE: 'paused',
-
-        // On ADD_MINUTE, increment context.duration by 60 seconds
-        // ...
+        ADD_MINUTE: {
+          actions: assign({
+            duration: (context, event) => context.duration + 60
+          })
+        }
       },
     },
     paused: {
